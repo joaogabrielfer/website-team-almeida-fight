0f07ef0460284a7b2eae0205fed3333c39bb97cd
João Gabriel <joaoggabriel07@gmail.com>
2026-09-02 12:16:02 -0300

    Refine instructor cutout imagery

--- client/data/upscaledInstructorImages.ts
+++ client/data/upscaledInstructorImages.ts
@@ -1,6 +1,6 @@
 export const UPSCALED_INSTRUCTOR_IMAGES = {
   caioHenrique: "/images/upscaled/instructors/caioHenrique-cutout.png",
-  cesarAugusto: "/images/upscaled/instructors/cesarAugusto-cutout.png",
+  cesarAugusto: "/images/upscaled/instructors/cesarAugusto.png",
   ismaelNinja: "/images/upscaled/instructors/ismaelNinja-cutout.png",
   italoFeijo: "/images/upscaled/instructors/italoFeijo-cutout.png",
   thiagoAlmeida: "/images/upscaled/instructors/thiagoAlmeida-cutout.png",
